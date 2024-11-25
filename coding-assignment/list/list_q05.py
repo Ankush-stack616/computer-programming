5ab9b18aa700ea4cc098058c9d91b47fd56aa5ff
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:07:05 +0530

    Update list_q05.py

--- coding-assignment/list/list_q05.py
+++ coding-assignment/list/list_q05.py
@@ -1,1 +1,4 @@
+# Write a program to remove duplicate elements from a list.
 
+lst = list(map(int, input("Enter numbers separated by spaces: ").split()))
+print("List without duplicates:", list(set(lst)))
