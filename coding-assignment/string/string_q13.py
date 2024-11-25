2db29905503d48a0f20cad1621cbc658937b2c31
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:34:39 +0530

    Update string_q13.py

--- coding-assignment/string/string_q13.py
+++ coding-assignment/string/string_q13.py
@@ -1,1 +1,16 @@
+#  Write a program to check if a string contains only unique characters.
 
+s = input("Enter a string: ")
+char_set = set()
+
+unique = True
+for char in s:
+    if char in char_set:
+        unique = False
+        break
+    char_set.add(char)
+
+if unique:
+    print("The string has all unique characters.")
+else:
+    print("The string does not have all unique characters.")
