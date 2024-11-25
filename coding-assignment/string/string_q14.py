7a369b8559c52bd7b73a1136b467c66c66821c55
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:35:06 +0530

    Update string_q14.py

--- coding-assignment/string/string_q14.py
+++ coding-assignment/string/string_q14.py
@@ -1,1 +1,14 @@
+# Write a program to find all possible substrings of a string.
 
+s = input("Enter a string: ")
+
+n = len(s)
+substrings = []
+
+for i in range(n):
+    for j in range(i+1, n+1):
+        substrings.append(s[i:j])
+
+print("All substrings:")
+for substring in substrings:
+    print(substring)
