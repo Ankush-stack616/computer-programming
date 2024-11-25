b4f761406b380a96034b02a615b50b64b3d0d69b
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:09:25 +0530

    Update list_q10.py

--- coding-assignment/list/list_q10.py
+++ coding-assignment/list/list_q10.py
@@ -1,1 +1,4 @@
+# Write a program to input a list of numbers and sort it in ascending order.
 
+lst = list(map(int, input("Enter numbers separated by spaces: ").split()))
+print("Sorted list:", sorted(lst))
