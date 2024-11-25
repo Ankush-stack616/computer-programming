9686cb7109eb5dc9aa3f38d2fbe883aecb6de93f
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:17:59 +0530

    Update list_q19.py

--- coding-assignment/list/list_q19.py
+++ coding-assignment/list/list_q19.py
@@ -1,1 +1,9 @@
+# Write a program to compute the cumulative sum of a list.
 
+lst = list(map(int, input("Enter numbers separated by spaces: ").split()))
+cumulative_sum = []
+total = 0
+for num in lst:
+    total += num
+    cumulative_sum.append(total)
+print("Cumulative sum:", cumulative_sum)
