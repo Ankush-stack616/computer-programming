7909c55a66fd45c0b887da60ffc7b8aca055f90f
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:38:15 +0530

    Update function_q04.py

--- coding-assignment/function/function_q04.py
+++ coding-assignment/function/function_q04.py
@@ -1,1 +1,10 @@
+# Write a function to print the first n Fibonacci numbers.
 
+def fibonacci(n):
+    fib = [0, 1]
+    for i in range(2, n):
+        fib.append(fib[-1] + fib[-2])
+    return fib[:n]
+
+num = int(input("Enter the number of terms: "))
+print("Fibonacci series:", fibonacci(num))
