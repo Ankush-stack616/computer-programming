5148e3008de30ede139f04ad660c91605e99902c
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:54:23 +0530

    Update function_q19.py

--- coding-assignment/function/function_q19.py
+++ coding-assignment/function/function_q19.py
@@ -1,1 +1,11 @@
+# Write a function to find the HCF and LCM of two numbers.
 
+def hcf_and_lcm(a, b):
+    hcf = gcd(a, b)
+    lcm = abs(a * b) // hcf
+    return hcf, lcm
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+hcf, lcm = hcf_and_lcm(a, b)
+print("HCF:", hcf, "LCM:", lcm)
