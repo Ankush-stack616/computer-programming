052b0c143f3d9792b87513347fc819fe62c14283
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:21:20 +0530

    Update dict_q09.py

--- coding-assignment/dict/dict_q09.py
+++ coding-assignment/dict/dict_q09.py
@@ -1,1 +1,7 @@
+# Count the frequency of digits in a number.
 
+number = input("Enter a number: ")
+digit_count = {}
+for digit in number:
+    digit_count[digit] = digit_count.get(digit, 0) + 1
+print("Digit frequency:", digit_count)
