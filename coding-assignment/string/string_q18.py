80be368363975938822a54adbcd19ff6f1de1d45
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:38:03 +0530

    Update string_q18.py

--- coding-assignment/string/string_q18.py
+++ coding-assignment/string/string_q18.py
@@ -1,1 +1,11 @@
+# Write a program to capitalize the first letter of each word in a sentence.
 
+sentence = input("Enter a sentence: ")
+words = sentence.split()
+capitalized_words = []
+
+for word in words:
+    capitalized_words.append(word.capitalize())
+
+result = " ".join(capitalized_words)
+print("Capitalized sentence:", result)
