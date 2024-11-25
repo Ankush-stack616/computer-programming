2b61ab19740836197cdc3fee95f2dfc7432241b0
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:13:52 +0530

    Update list_q13.py

--- coding-assignment/list/list_q13.py
+++ coding-assignment/list/list_q13.py
@@ -1,1 +1,6 @@
+# Write a program to calculate the median of a list of numbers.
 
+lst = list(map(int, input("Enter numbers separated by spaces: ").split()))
+lst.sort()
+n = len(lst)
+print("Median:", (lst[n // 2 - 1] + lst[n // 2]) / 2 if n % 2 == 0 else lst[n // 2])
