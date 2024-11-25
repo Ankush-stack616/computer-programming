6462002ff5a3602acc1060f1afd9fc45d257d832
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:32:38 +0530

    Update string_q09.py

--- coding-assignment/string/string_q09.py
+++ coding-assignment/string/string_q09.py
@@ -1,1 +1,13 @@
+# Write a program to replace all vowels in a string with *.
 
+s = input("Enter a string: ")
+vowels = "aeiouAEIOU"
+result = ""
+
+for char in s:
+    if char in vowels:
+        result += "*"
+    else:
+        result += char
+
+print("Modified string:", result)
