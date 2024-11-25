e83b487488b23e16b209419a49965846a36fd132
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:15:59 +0530

    Update dict_q05.py

--- coding-assignment/dict/dict_q05.py
+++ coding-assignment/dict/dict_q05.py
@@ -1,1 +1,7 @@
+# Group elements of a list by their frequency using a dictionary.
 
+arr = list(map(int, input("Enter a list of numbers: ").split()))
+freq_dict = {}
+for num in arr:
+    freq_dict[num] = freq_dict.get(num, 0) + 1
+print("Grouped by frequency:", freq_dict)
