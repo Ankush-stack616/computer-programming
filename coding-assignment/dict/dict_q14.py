223302a3628a92ea2f156d5e0ed5b97adda129d6
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:25:57 +0530

    Update dict_q14.py

--- coding-assignment/dict/dict_q14.py
+++ coding-assignment/dict/dict_q14.py
@@ -1,1 +1,5 @@
+# Reverse keys and values in a dictionary.
 
+dictionary = {'a': 1, 'b': 2, 'c': 3}
+reversed_dict = {v: k for k, v in dictionary.items()}
+print("Reversed dictionary:", reversed_dict)
