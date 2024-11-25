c7cdd0fc0318a5fd38d3ad339ca4221bfc72ea3f
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:26:23 +0530

    Update dict_q15.py

--- coding-assignment/dict/dict_q15.py
+++ coding-assignment/dict/dict_q15.py
@@ -1,1 +1,6 @@
+# Create a dictionary from two lists (keys and values).
 
+keys = input("Enter keys separated by spaces: ").split()
+values = input("Enter values separated by spaces: ").split()
+dictionary = dict(zip(keys, values))
+print("Dictionary:", dictionary)
