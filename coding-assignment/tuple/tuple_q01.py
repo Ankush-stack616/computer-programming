74b333a3df34178701b6d8dcb7a7af12520423b1
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:44:34 +0530

    Update tuple_q01.py

--- coding-assignment/tuple/tuple_q01.py
+++ coding-assignment/tuple/tuple_q01.py
@@ -1,1 +1,11 @@
+#  Take multiple inputs from the user and store them in a tuple, then print the tuple.
 
+n = int(input("Enter the number of elements: "))
+elements = []
+
+for i in range(n):
+    ele = input(f"Enter element {i+1}: ")
+    elements.append(ele)
+
+result_tuple = tuple(elements)
+print("Created Tuple:", result_tuple)
