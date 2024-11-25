2923bc627d768079f6f001f3755fcb6c197b4cfb
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:57:40 +0530

    Update tuple_q17.py

--- coding-assignment/tuple/tuple_q17.py
+++ coding-assignment/tuple/tuple_q17.py
@@ -1,1 +1,7 @@
+# Duplicate a tuple n times as specified by the user.
 
+values = tuple(input("Enter tuple elements separated by spaces: ").split())
+n = int(input("Enter the number of times to duplicate the tuple: "))
+
+duplicated_tuple = values * n
+print("Duplicated Tuple:", duplicated_tuple)
