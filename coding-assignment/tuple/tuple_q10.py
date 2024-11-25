bd74b86e7652f963325dd99e899e491dcc041446
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:54:43 +0530

    Update tuple_q10.py

--- coding-assignment/tuple/tuple_q10.py
+++ coding-assignment/tuple/tuple_q10.py
@@ -1,1 +1,5 @@
+#  Convert a list input by the user into a tuple.
 
+elements = input("Enter list elements separated by spaces: ").split()
+result_tuple = tuple(elements)
+print("Converted Tuple:", result_tuple)
