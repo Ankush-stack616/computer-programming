4caea4cce5ac3e1cb3241fb31c3698687773cc2d
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:06:41 +0530

    Update list_q04.py

--- coding-assignment/list/list_q04.py
+++ coding-assignment/list/list_q04.py
@@ -1,1 +1,7 @@
+# Write a program to input a list and display the frequency of each element in it.
 
+lst = list(map(int, input("Enter numbers separated by spaces: ").split()))
+freq = {}
+for num in lst:
+    freq[num] = freq.get(num, 0) + 1
+print("Frequency:", freq)
