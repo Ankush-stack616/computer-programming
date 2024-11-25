f66f4e9d2f11a3db2b42d47c5bfbcccc6cc400ea
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:12:02 +0530

    Update list_q12.py

--- coding-assignment/list/list_q12.py
+++ coding-assignment/list/list_q12.py
@@ -1,1 +1,5 @@
+# Write a program to rotate a list k steps to the right.
 
+lst = list(map(int, input("Enter numbers separated by spaces: ").split()))
+k = int(input("Enter rotation value: ")) % len(lst)
+print("Rotated list:", lst[-k:] + lst[:-k])
