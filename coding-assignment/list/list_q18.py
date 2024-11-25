959691e446716a530830e4201de6a5b6c9c6ced7
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:16:56 +0530

    Update list_q18.py

--- coding-assignment/list/list_q18.py
+++ coding-assignment/list/list_q18.py
@@ -1,1 +1,5 @@
+# Write a program to count the number of negative numbers in a list.
 
+lst = list(map(int, input("Enter numbers separated by spaces: ").split()))
+negatives = sum(1 for num in lst if num < 0)
+print("Count of negatives:", negatives)
