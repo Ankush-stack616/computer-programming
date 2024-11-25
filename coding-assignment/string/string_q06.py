a5cac64e0c11590fe2d2cd23516d35fcfe35344b
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:30:51 +0530

    Update string_q06.py

--- coding-assignment/string/string_q06.py
+++ coding-assignment/string/string_q06.py
@@ -1,1 +1,14 @@
+# Write a program to check if two strings are anagrams.
 
+s1 = input("Enter the first string: ").lower()
+s2 = input("Enter the second string: ").lower()
+
+if len(s1) != len(s2):
+    print("The strings are not anagrams (length mismatch).")
+else:
+    sorted_s1 = sorted(s1)
+    sorted_s2 = sorted(s2)
+    if sorted_s1 == sorted_s2:
+        print("The strings are anagrams!")
+    else:
+        print("The strings are not anagrams.")
