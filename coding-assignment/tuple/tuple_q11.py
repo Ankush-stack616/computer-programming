773f6628b5e52856a6cf0a87be2f75552a094db6
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:55:04 +0530

    Update tuple_q11.py

--- coding-assignment/tuple/tuple_q11.py
+++ coding-assignment/tuple/tuple_q11.py
@@ -1,1 +1,11 @@
+#  Unpack the elements of a tuple into individual variables.
 
+values = tuple(input("Enter tuple elements separated by spaces: ").split())
+
+if len(values) >= 3:
+    a, b, c = values[0], values[1], values[2]
+    print("First element:", a)
+    print("Second element:", b)
+    print("Third element:", c)
+else:
+    print("Tuple does not have enough elements to unpack!")
