a9fd019297e129bb31e17faffc92758f7bca7261
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:16:17 +0530

    Update list_q17.py

--- coding-assignment/list/list_q17.py
+++ coding-assignment/list/list_q17.py
@@ -1,1 +1,6 @@
+# Write a program to calculate the product of all elements in a list.
 
+from functools import reduce
+lst = list(map(int, input("Enter numbers separated by spaces: ").split()))
+product = reduce(lambda x, y: x * y, lst)
+print("Product of elements:", product)
