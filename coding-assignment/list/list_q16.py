cdf69911a1988adb2e62852abc47b5365928da5e
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:15:12 +0530

    Update list_q16.py

--- coding-assignment/list/list_q16.py
+++ coding-assignment/list/list_q16.py
@@ -1,1 +1,6 @@
+# Write a program to find elements that are unique to each of two lists.
 
+lst1 = list(map(int, input("Enter numbers for list 1: ").split()))
+lst2 = list(map(int, input("Enter numbers for list 2: ").split()))
+unique = list(set(lst1) ^ set(lst2))
+print("Unique elements:", unique)
