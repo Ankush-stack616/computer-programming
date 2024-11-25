7f4ec1fad80c7e7c6730e15047cfd6d10119fe16
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:29:01 +0530

    Update dict_q17.py

--- coding-assignment/dict/dict_q17.py
+++ coding-assignment/dict/dict_q17.py
@@ -1,1 +1,5 @@
+# Convert all keys of a dictionary to uppercase.
 
+dictionary = {'name': 'Alice', 'age': 30}
+uppercase_dict = {k.upper(): v for k, v in dictionary.items()}
+print("Uppercase dictionary:", uppercase_dict)
