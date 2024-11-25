734186f526583a30f8bb33ae4e32319b98f3fb4f
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:47:05 +0530

    Update function_q14.py

--- coding-assignment/function/function_q14.py
+++ coding-assignment/function/function_q14.py
@@ -1,1 +1,7 @@
+# Write a function to sort a list of numbers in ascending order.
 
+def sort_list(lst):
+    return sorted(lst)
+
+lst = list(map(int, input("Enter numbers separated by spaces: ").split()))
+print("Sorted list:", sort_list(lst))
