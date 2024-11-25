c26f1b0cfa39fae530b10bd15d88f1c32a28d94d
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:17:12 +0530

    Update dict_q07.py

--- coding-assignment/dict/dict_q07.py
+++ coding-assignment/dict/dict_q07.py
@@ -1,1 +1,6 @@
+# Check if one dictionary is a subset of another.
 
+dict1 = eval(input("Enter the first dictionary: "))
+dict2 = eval(input("Enter the second dictionary: "))
+is_subset = all(item in dict2.items() for item in dict1.items())
+print("Is subset:", is_subset)
