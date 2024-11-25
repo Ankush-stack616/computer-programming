23454ac8e19e43da266102387d88413d60f32f3f
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:37:09 +0530

    Update string_q16.py

--- coding-assignment/string/string_q16.py
+++ coding-assignment/string/string_q16.py
@@ -1,1 +1,12 @@
+# Write a program to remove all duplicate characters from a string.
 
+s = input("Enter a string: ")
+seen = set()
+result = ""
+
+for char in s:
+    if char not in seen:
+        result += char
+        seen.add(char)
+
+print("String after removing duplicates:", result)
