3e861d8c96474bccb6d11d150f82fbbe92c78898
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:40:39 +0530

    Update function_q07.py

--- coding-assignment/function/function_q07.py
+++ coding-assignment/function/function_q07.py
@@ -1,1 +1,8 @@
+# Write a function to check if a number is an Armstrong number.
 
+def is_armstrong(n):
+    digits = [int(d) for d in str(n)]
+    return sum(d ** len(digits) for d in digits) == n
+
+num = int(input("Enter a number: "))
+print("Is Armstrong number:", is_armstrong(num))
