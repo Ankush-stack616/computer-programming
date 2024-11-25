3ce02bd73eead3550bfe9e07c510dd86a6343d4c
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:46:24 +0530

    Update function_q13.py

--- coding-assignment/function/function_q13.py
+++ coding-assignment/function/function_q13.py
@@ -1,1 +1,8 @@
+# Write a function to find the missing number in a sequence.
 
+def find_missing(lst, n):
+    return sum(range(1, n+1)) - sum(lst)
+
+lst = list(map(int, input("Enter the sequence with one missing number: ").split()))
+n = int(input("Enter the expected size of the sequence: "))
+print("Missing number:", find_missing(lst, n))
