7a897f7b080b7a7125cb5c5a3d7bb12a7f2ba58e
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:59:07 +0530

    Update tuple_q20.py

--- coding-assignment/tuple/tuple_q20.py
+++ coding-assignment/tuple/tuple_q20.py
@@ -1,1 +1,7 @@
+#  Find elements in one tuple that are not in another.
 
+tuple1 = tuple(input("Enter elements of the first tuple separated by spaces: ").split())
+tuple2 = tuple(input("Enter elements of the second tuple separated by spaces: ").split())
+
+difference = set(tuple1).difference(tuple2)
+print("Elements in first tuple but not in second:", tuple(difference))
