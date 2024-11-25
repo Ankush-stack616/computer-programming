4f4048dbbf30c303ac08b0eca89d543a815e6844
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:47:58 +0530

    Update function_q15.py

--- coding-assignment/function/function_q15.py
+++ coding-assignment/function/function_q15.py
@@ -1,1 +1,11 @@
+# Write a function to generate all prime numbers up to n.
 
+def generate_primes(n):
+    primes = []
+    for i in range(2, n+1):
+        if all(i % p != 0 for p in primes):
+            primes.append(i)
+    return primes
+
+n = int(input("Enter a number: "))
+print("Prime numbers:", generate_primes(n))
