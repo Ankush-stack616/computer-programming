1f7cd33266bd85a5d8557d8a9f4ff9af28e1d85e
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:40:12 +0530

    Update function_q06.py

--- coding-assignment/function/function_q06.py
+++ coding-assignment/function/function_q06.py
@@ -1,1 +1,7 @@
+# Write a function to find the maximum element in a list.
 
+def find_max(lst):
+    return max(lst)
+
+lst = list(map(int, input("Enter numbers separated by spaces: ").split()))
+print("Maximum element:", find_max(lst))
