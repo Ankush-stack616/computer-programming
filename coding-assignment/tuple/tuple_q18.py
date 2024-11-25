df6f7da9e9101a3933c7521f6221e8f14b1df0a6
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:58:03 +0530

    Update tuple_q18.py

--- coding-assignment/tuple/tuple_q18.py
+++ coding-assignment/tuple/tuple_q18.py
@@ -1,1 +1,6 @@
+# Convert a tuple of characters to a string.
 
+values = tuple(input("Enter characters separated by spaces: ").split())
+result_string = ''.join(values)
+
+print("Converted String:", result_string)
