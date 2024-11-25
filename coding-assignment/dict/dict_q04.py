35f8adae4e94c033cc92da0e7f4b71ade60a3d5d
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:15:25 +0530

    Update dict_q04.py

--- coding-assignment/dict/dict_q04.py
+++ coding-assignment/dict/dict_q04.py
@@ -1,1 +1,9 @@
+# Find the top N most frequent elements in a list.
 
+from collections import Counter
+
+arr = list(map(int, input("Enter a list of numbers: ").split()))
+n = int(input("Enter the value of N: "))
+freq = Counter(arr)
+top_n = freq.most_common(n)
+print("Top N frequent elements:", top_n)
