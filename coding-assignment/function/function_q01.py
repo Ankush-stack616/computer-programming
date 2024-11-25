f55cd849b5a52d72354b69312c1279c49e942cd0
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:36:06 +0530

    Update function_q01.py

--- coding-assignment/function/function_q01.py
+++ coding-assignment/function/function_q01.py
@@ -1,1 +1,9 @@
+# Write a function to calculate the factorial of a number.
 
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+num = int(input("Enter a number: "))
+print("Factorial:", factorial(num))
