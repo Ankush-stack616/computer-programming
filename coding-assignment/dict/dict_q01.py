6edc6b53f3c51ba208eddc9360b78936d095f057
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:12:17 +0530

    Update dict_q01.py

--- coding-assignment/dict/dict_q01.py
+++ coding-assignment/dict/dict_q01.py
@@ -1,1 +1,7 @@
-
+# Write a program to count the frequency of words in a given string.
+string = input("Enter a string: ")
+words = string.split()
+word_count = {}
+for word in words:
+    word_count[word] = word_count.get(word, 0) + 1
+print("Word frequency:", word_count)
