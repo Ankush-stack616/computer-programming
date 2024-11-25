c2936af7d2785091d5db11acce27b659aa201c37
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:08:31 +0530

    Update list_q08.py

--- coding-assignment/list/list_q08.py
+++ coding-assignment/list/list_q08.py
@@ -1,1 +1,5 @@
+# Write a program to filter and display all even numbers from a list.
 
+lst = list(map(int, input("Enter numbers separated by spaces: ").split()))
+evens = [num for num in lst if num % 2 == 0]
+print("Even numbers:", evens)
