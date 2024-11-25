c0cef279f7c604de38d5d3d20b14b30a36e3ce75
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:27:12 +0530

    Update string_q04.py

--- coding-assignment/string/string_q04.py
+++ coding-assignment/string/string_q04.py
@@ -1,1 +1,7 @@
+# Write a program to find the frequency of each character in a string.
 
+s = input("Enter a string: ")
+freq = {}
+for char in s:
+    freq[char] = freq.get(char, 0) + 1
+print("Character frequencies:", freq)
