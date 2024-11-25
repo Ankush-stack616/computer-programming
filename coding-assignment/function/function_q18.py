a24e3e0cf3ef66f08d933b2ee05265fc65813fdf
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:53:28 +0530

    Update function_q18.py

--- coding-assignment/function/function_q18.py
+++ coding-assignment/function/function_q18.py
@@ -1,1 +1,8 @@
+# Write a function to check if a number is a perfect number.
 
+def is_perfect(n):
+    divisors = [i for i in range(1, n) if n % i == 0]
+    return sum(divisors) == n
+
+num = int(input("Enter a number: "))
+print("Is perfect number:", is_perfect(num))
