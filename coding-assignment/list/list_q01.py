2b7e405d33662bb120c96b175efe304a447df50d
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:04:40 +0530

    Update list_q01.py

--- coding-assignment/list/list_q01.py
+++ coding-assignment/list/list_q01.py
@@ -1,1 +1,4 @@
+ # Write a program to input a list of numbers and find the largest element in the list.
 
+lst = list(map(int, input("Enter numbers separated by spaces: ").split()))
+print("Maximum:", max(lst))
