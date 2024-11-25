50928ad0d514646ab484b730ea77f9dd36b5c614
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:56:43 +0530

    Update tuple_q15.py

--- coding-assignment/tuple/tuple_q15.py
+++ coding-assignment/tuple/tuple_q15.py
@@ -1,1 +1,6 @@
+# Input a tuple of integers and sort it in ascending order.
 
+values = tuple(map(int, input("Enter integers separated by spaces: ").split()))
+sorted_tuple = tuple(sorted(values))
+
+print("Sorted Tuple:", sorted_tuple)
