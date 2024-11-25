c73d41eb59c8ef2c9e5bd6f87c1722a02e2b7c8b
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:06:11 +0530

    Update list_q03.py

--- coding-assignment/list/list_q03.py
+++ coding-assignment/list/list_q03.py
@@ -1,1 +1,4 @@
+# Write a program to reverse a list of numbers and display the reversed list.
 
+lst = list(map(int, input("Enter numbers separated by spaces: ").split()))
+print("Reversed list:", lst[::-1])
