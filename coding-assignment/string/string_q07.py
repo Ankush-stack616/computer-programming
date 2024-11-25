bef115be2065aad671e4f8fc7a36646ca2d2a70d
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:31:26 +0530

    Update string_q07.py

--- coding-assignment/string/string_q07.py
+++ coding-assignment/string/string_q07.py
@@ -1,1 +1,17 @@
+#  Write a program to find the first non-repeating character in a string.
 
+s = input("Enter a string: ")
+char_count = {}
+
+for char in s:
+    char_count[char] = char_count.get(char, 0) + 1
+
+found = False
+for char in s:
+    if char_count[char] == 1:
+        print("First non-repeating character:", char)
+        found = True
+        break
+
+if not found:
+    print("No non-repeating character found.")
