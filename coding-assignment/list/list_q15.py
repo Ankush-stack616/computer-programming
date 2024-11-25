d0d6da4240f0aa2a029160dae10ba7f25ed7776f
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:14:41 +0530

    Update list_q15.py

--- coding-assignment/list/list_q15.py
+++ coding-assignment/list/list_q15.py
@@ -1,1 +1,6 @@
+#  Write a program to find common elements between two lists.
 
+lst1 = list(map(int, input("Enter numbers for list 1: ").split()))
+lst2 = list(map(int, input("Enter numbers for list 2: ").split()))
+common = list(set(lst1) & set(lst2))
+print("Common elements:", common)
