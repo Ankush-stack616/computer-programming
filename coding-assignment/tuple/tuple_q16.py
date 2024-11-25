c1b1abb61897f2b6e6fbd7dfd9c05703be35c611
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:57:16 +0530

    Update tuple_q16.py

--- coding-assignment/tuple/tuple_q16.py
+++ coding-assignment/tuple/tuple_q16.py
@@ -1,1 +1,7 @@
+# Input two lists and combine them into a tuple of pairs.
 
+list1 = input("Enter first list elements separated by spaces: ").split()
+list2 = input("Enter second list elements separated by spaces: ").split()
+
+result_tuple = tuple(zip(list1, list2))
+print("Tuple of pairs:", result_tuple)
