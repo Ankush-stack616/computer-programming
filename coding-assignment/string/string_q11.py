4fddd1eaf70a7114f38b503efc1e598823934143
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:33:44 +0530

    Update string_q11.py

--- coding-assignment/string/string_q11.py
+++ coding-assignment/string/string_q11.py
@@ -1,1 +1,17 @@
+# Write a program to find the longest palindromic substring in a string.
 
+s = input("Enter a string: ")
+
+def is_palindrome(substring):
+    return substring == substring[::-1]
+
+longest = ""
+n = len(s)
+
+for i in range(n):
+    for j in range(i, n):
+        substring = s[i:j+1]
+        if is_palindrome(substring) and len(substring) > len(longest):
+            longest = substring
+
+print("Longest palindromic substring:", longest)
