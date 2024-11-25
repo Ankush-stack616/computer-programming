471f005d885a2cec5afec8c0384b1d87ec2167d3
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:08:55 +0530

    Update list_q09.py

--- coding-assignment/list/list_q09.py
+++ coding-assignment/list/list_q09.py
@@ -1,1 +1,5 @@
+# Write a program to filter and display all odd numbers from a list.
 
+lst = list(map(int, input("Enter numbers separated by spaces: ").split()))
+odds = [num for num in lst if num % 2 != 0]
+print("Odd numbers:", odds)
