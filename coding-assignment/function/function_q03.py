bce2cd2da49d0b0598f2c7af1cb4ea9ef4b5b63b
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:37:49 +0530

    Update function_q03.py

--- coding-assignment/function/function_q03.py
+++ coding-assignment/function/function_q03.py
@@ -1,1 +1,7 @@
+# Write a function to find the sum of the digits of a number.
 
+def sum_of_digits(n):
+    return sum(int(digit) for digit in str(n))
+
+num = int(input("Enter a number: "))
+print("Sum of digits:", sum_of_digits(num))
