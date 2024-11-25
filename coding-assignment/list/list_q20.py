d30df96c71e810de760764c276f49b439b32f8c9
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:18:31 +0530

    Update list_q20.py

--- coding-assignment/list/list_q20.py
+++ coding-assignment/list/list_q20.py
@@ -1,1 +1,7 @@
+# Write a program to partition a list into two separate lists of even and odd numbers.
 
+lst = list(map(int, input("Enter numbers separated by spaces: ").split()))
+evens = [num for num in lst if num % 2 == 0]
+odds = [num for num in lst if num % 2 != 0]
+print("Even numbers:", evens)
+print("Odd numbers:", odds)
