f17c67e4d2cf03a4c8693011a8acbf1e993f195e
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:10:16 +0530

    Update list_q11.py

--- coding-assignment/list/list_q11.py
+++ coding-assignment/list/list_q11.py
@@ -1,1 +1,5 @@
+# Write a program to input a list and find the index of a specific element.
 
+lst = list(map(int, input("Enter numbers separated by spaces: ").split()))
+x = int(input("Enter the element to find: "))
+print("Index:", lst.index(x) if x in lst else -1)
