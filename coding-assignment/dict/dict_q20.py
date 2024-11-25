f98fd1f3a3534e72bd636305801310b8a7b49df5
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:32:36 +0530

    Update dict_q20.py

--- coding-assignment/dict/dict_q20.py
+++ coding-assignment/dict/dict_q20.py
@@ -1,1 +1,5 @@
+# Sort a dictionary based on the length of its keys.
 
+dictionary = {'apple': 10, 'pear': 20, 'banana': 5}
+sorted_dict = dict(sorted(dictionary.items(), key=lambda x: len(x[0])))
+print("Sorted dictionary:", sorted_dict)
