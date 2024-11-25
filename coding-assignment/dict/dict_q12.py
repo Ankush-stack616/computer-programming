bbd8e9c468ede3ecc5413bdeb8d633722bcaf9cd
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:24:40 +0530

    Update dict_q12.py

--- coding-assignment/dict/dict_q12.py
+++ coding-assignment/dict/dict_q12.py
@@ -1,1 +1,6 @@
+# Filter out keys starting with a specific character.
 
+dictionary = {'apple': 1, 'banana': 2, 'cherry': 3, 'apricot': 4}
+start = input("Enter starting character: ")
+filtered_dict = {k: v for k, v in dictionary.items() if not k.startswith(start)}
+print("Filtered dictionary:", filtered_dict)
