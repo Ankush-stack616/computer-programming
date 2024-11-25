f4089391307c5e89166b5049075c71ddcd266c86
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:46:35 +0530

    Update tuple_q05.py

--- coding-assignment/tuple/tuple_q05.py
+++ coding-assignment/tuple/tuple_q05.py
@@ -1,1 +1,7 @@
+# Create two tuples from user input and concatenate them.
 
+tuple1 = tuple(input("Enter elements for the first tuple separated by spaces: ").split())
+tuple2 = tuple(input("Enter elements for the second tuple separated by spaces: ").split())
+
+result_tuple = tuple1 + tuple2
+print("Concatenated Tuple:", result_tuple)
