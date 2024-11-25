2d5055e6c5f8971dc873a0a0899a395449f724a7
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:38:56 +0530

    Update function_q05.py

--- coding-assignment/function/function_q05.py
+++ coding-assignment/function/function_q05.py
@@ -1,1 +1,7 @@
+# Write a function to check if a string is a palindrome.
 
+def is_palindrome(s):
+    return s == s[::-1]
+
+string = input("Enter a string: ")
+print("Is palindrome:", is_palindrome(string))
