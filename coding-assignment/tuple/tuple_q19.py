45815347c62b2f2cb3fd06d8b945c57062551c6e
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:58:38 +0530

    Update tuple_q19.py

--- coding-assignment/tuple/tuple_q19.py
+++ coding-assignment/tuple/tuple_q19.py
@@ -1,1 +1,7 @@
+# Input two tuples and find the common elements.
 
+tuple1 = tuple(input("Enter elements of the first tuple separated by spaces: ").split())
+tuple2 = tuple(input("Enter elements of the second tuple separated by spaces: ").split())
+
+common_elements = set(tuple1).intersection(tuple2)
+print("Common Elements:", tuple(common_elements))
