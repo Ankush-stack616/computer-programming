15be59a0bdbc739de77f8d81dba6381aecc5297e
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:34:13 +0530

    Update string_q12.py

--- coding-assignment/string/string_q12.py
+++ coding-assignment/string/string_q12.py
@@ -1,1 +1,11 @@
+# Write a program to reverse each word in a given sentence.
 
+sentence = input("Enter a sentence: ")
+words = sentence.split()
+reversed_words = []
+
+for word in words:
+    reversed_words.append(word[::-1])
+
+result = " ".join(reversed_words)
+print("Reversed words sentence:", result)
