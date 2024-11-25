8a755faef70f4266b1567f11e39bbd11f3de9015
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 20:39:32 +0530

    Update string_q20.py

--- coding-assignment/string/string_q20.py
+++ coding-assignment/string/string_q20.py
@@ -1,1 +1,12 @@
+# Write a program to count all substrings that start and end with the same character.
 
+s = input("Enter a string: ")
+n = len(s)
+count = 0
+
+for i in range(n):
+    for j in range(i, n):
+        if s[i] == s[j]:
+            count += 1
+
+print("Number of substrings starting and ending with the same character:", count)
