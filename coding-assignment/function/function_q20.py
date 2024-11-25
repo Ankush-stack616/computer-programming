3ada380b48b41ff2f0775c4e9e06d3526b2ee122
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:55:08 +0530

    Update function_q20.py

--- coding-assignment/function/function_q20.py
+++ coding-assignment/function/function_q20.py
@@ -1,1 +1,10 @@
+# Write a function to count the frequency of each element in a list.
 
+def count_frequency(lst):
+    freq = {}
+    for num in lst:
+        freq[num] = freq.get(num, 0) + 1
+    return freq
+
+lst = list(map(int, input("Enter numbers separated by spaces: ").split()))
+print("Frequency count:", count_frequency(lst))
