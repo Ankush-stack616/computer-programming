ed7a434ba58f991b57d9f202458f9cfb008f9efa
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:14:50 +0530

    Update dict_q03.py

--- coding-assignment/dict/dict_q03.py
+++ coding-assignment/dict/dict_q03.py
@@ -1,1 +1,9 @@
+# Write a function to merge two dictionaries.
+
+def merge_dicts(dict1, dict2):
+    return {**dict1, **dict2}
+
+dict1 = eval(input("Enter the first dictionary: "))
+dict2 = eval(input("Enter the second dictionary: "))
+print("Merged dictionary:", merge_dicts(dict1, dict2))
 
