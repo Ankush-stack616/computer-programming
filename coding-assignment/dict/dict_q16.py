7aaab89ac00de90f7c9abf006673f3c2dd9d1e53
Ankush-stack616 <ankushshakya1221@gmail.com>
2024-11-25 19:28:38 +0530

    Update dict_q16.py

--- coding-assignment/dict/dict_q16.py
+++ coding-assignment/dict/dict_q16.py
@@ -1,1 +1,8 @@
+# Combine dictionaries by summing values of common keys.
 
+from collections import Counter
+
+dict1 = {'a': 10, 'b': 20}
+dict2 = {'a': 15, 'c': 25}
+combined = dict(Counter(dict1) + Counter(dict2))
+print("Combined dictionary:", combined)
